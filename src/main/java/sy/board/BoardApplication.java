39128336ca07cd1df1a1b1a14cd1cdf89bfdca82
SYnote <cwan13@naver.com>
2025-09-27 23:40:25 +0900

    chore(app): enable @MapperScan for MyBatis mappers

--- src/main/java/sy/board/BoardApplication.java
+++ src/main/java/sy/board/BoardApplication.java
@@ -1,9 +1,11 @@
 package sy.board;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("sy.board.mapper")
 public class BoardApplication {
 
 	public static void main(String[] args) {
